615465107511afa6a361d1488ebbb89f1fc9d5ae
yotak <20190998@sungshin.ac.kr>
2022-05-03 09:08:45 +0900

    [add] assignment_jekim

---
+++ Queue/LinkedQueue & ArrayQueue/includes/linkeddeque.h
@@ -0,0 +1,40 @@
+#ifndef _LINKED_DEQUE_
+#define _LINKED_DEQUE_
+
+typedef struct DequeNodeType
+{
+	char data;
+	struct DequeNodeType* pRLink;
+	struct DequeNodeType* pLLink;
+} DequeNode;
+
+typedef struct LinkedDequeType
+{
+	int currentElementCount;	// ���� ������ ����
+	DequeNode* pFrontNode;		// Front ����� ������
+	DequeNode* pRearNode;		// Rear ����� ������
+} LinkedDeque;
+
+LinkedDeque* createLinkedDeque();
+DequeNode* createLDNode(char input);
+int insertFrontLD(LinkedDeque* pDeque, DequeNode element);
+int insertRearLD(LinkedDeque* pDeque, DequeNode element);
+DequeNode* deleteFrontLD(LinkedDeque* pDeque);
+DequeNode* deleteRearLD(LinkedDeque* pDeque);
+DequeNode* peekFrontLD(LinkedDeque* pDeque);
+DequeNode* peekRearLD(LinkedDeque* pDeque);
+DequeNode* createLDNode(char input);
+void deleteLinkedDeque(LinkedDeque* pDeque);
+int isLinkedDequeFull(LinkedDeque* pDeque);
+int isLinkedDequeEmpty(LinkedDeque* pDeque);
+void displayLD(LinkedDeque* pDeque);
+
+#endif
+
+#ifndef _COMMON_QUEUE_DEF_
+#define _COMMON_QUEUE_DEF_
+
+#define TRUE		1
+#define FALSE		0
+
+#endif
