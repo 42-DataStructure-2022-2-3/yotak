b13548f67f4c658e97795081a46962234963b47c
yotak <20190998@sungshin.ac.kr>
2022-05-27 09:55:58 +0900

    feat: main.c

---
+++ 05Graph/graphlinkedlist/main.c
@@ -0,0 +1,29 @@
+#include "graphlinkedlist.h"
+
+int main(void)
+{
+	int i = 0;
+	LinkedGraph *lg1;
+
+	lg1 = createLinkedDirectedGraph(6);
+
+	for(; i < 6; i++)
+		addVertexLG(lg1, i);
+
+	addEdgeLG(lg1, 0, 1);
+	addEdgeLG(lg1, 1, 2);
+	addEdgeLG(lg1, 2, 1);
+	addEdgeLG(lg1, 2, 3);
+	addEdgeLG(lg1, 3, 2);
+	addEdgeLG(lg1, 3, 4);
+	addEdgeLG(lg1, 4, 5);
+	addEdgeLG(lg1, 5, 3);
+
+	printf("G1: \n");
+	displayLinkedGraph(lg1);
+
+	deleteLinkedGraph(lg1);
+	free(lg1);
+	system("leaks linkedgraph");
+	return 0;	
+}
